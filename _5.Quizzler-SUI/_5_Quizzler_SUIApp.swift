d311426d2a399d68663324f1fe21bc39bb4ea3f9
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-01-31 15:36:26 +0300

    Initial Commit

---
+++ _5.Quizzler-SUI/_5_Quizzler_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_Quizzler_SUIApp.swift
+//  _5.Quizzler-SUI
+//
+//  Created by Андрей Бородкин on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct _5_Quizzler_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
